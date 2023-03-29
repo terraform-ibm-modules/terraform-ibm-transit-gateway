d4dcb704113eaa7e6e35b22c4c87cd030ba5422e
valerio-bontempi <valerio.bontempi@it.ibm.com>
2023-03-29 13:04:19 +0200

    fix: modified provider apikey comment

--- examples/crossaccounts/provider.tf
+++ examples/crossaccounts/provider.tf
@@ -1,13 +1,13 @@
 # provider for account A
 provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key # pragma: allowlist secret - API key with access to RIAS APIs
+  ibmcloud_api_key = var.ibmcloud_api_key # pragma: transit gateway account owner apikey
   region           = var.region_a
   alias            = "accountA"
 }
 
 # provider for account B
 provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key_ext # pragma: allowlist secret - API key with access to RIAS APIs
+  ibmcloud_api_key = var.ibmcloud_api_key_ext # pragma: external account apikey
   region           = var.region_b
   alias            = "accountB"
 }
