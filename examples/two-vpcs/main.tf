519ea103c892f9eb0539b6258d0cec12052bbb94
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-05-05 04:10:12 +0100

    chore(deps): update terraform terraform-ibm-modules/landing-zone-vpc/ibm to v7.18.0 (#493)

--- examples/two-vpcs/main.tf
+++ examples/two-vpcs/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "vpc_1" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.17.1"
+  version           = "7.18.0"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
@@ -31,7 +31,7 @@ module "vpc_1" {
 
 module "vpc_2" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.17.1"
+  version           = "7.18.0"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
