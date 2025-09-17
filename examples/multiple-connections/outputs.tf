8f315a04a58af545a321ceea176b23549e79cd40
valerio-bontempi <valerio.bontempi@it.ibm.com>
2025-09-17 19:08:56 +0200

    fix: added output

--- examples/multiple-connections/outputs.tf
+++ examples/multiple-connections/outputs.tf
@@ -2,10 +2,12 @@
 # Outputs
 ##############################################################################
 
-#output "myoutput" {
-#  description = "Description of my output"
-#  value       = "value"
-#  depends_on  = [<some resource>]
-#}
+output "tg_gateway_connection_1" {
+  description = "Transit gateway approved connection #1 details"
+  value       = module.tg_gateway_connection_1
+}
 
-##############################################################################
+output "tg_gateway_connection_2" {
+  description = "Transit gateway approved connection #2 details"
+  value       = module.tg_gateway_connection_2
+}
