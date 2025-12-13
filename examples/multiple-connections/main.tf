ce1722623882ba296a89a8279ed171d6e0f31b1e
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-13 19:52:43 +0000

    fix(deps): update dependencies (#661)

--- examples/multiple-connections/main.tf
+++ examples/multiple-connections/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.4.3"
+  version = "1.4.6"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "vpc_1" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.10.0"
+  version           = "8.10.1"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
@@ -31,7 +31,7 @@ module "vpc_1" {
 
 module "vpc_2" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.10.0"
+  version           = "8.10.1"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
@@ -46,7 +46,7 @@ module "vpc_2" {
 
 module "vpc_3" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.10.0"
+  version           = "8.10.1"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
@@ -61,7 +61,7 @@ module "vpc_3" {
 
 module "vpc_4" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.10.0"
+  version           = "8.10.1"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
