a495a678a88bd3ebe867e82f97a25f2e3707236c
iamar7 <iamar7@users.noreply.github.com>
2026-06-23 11:08:13 +0530

    fix: standardize resource_tags variable, update descriptions and add access tag validation

--- variables.tf
+++ variables.tf
@@ -30,8 +30,12 @@ variable "existing_transit_gateway_name" {
 
 variable "resource_tags" {
   type        = list(string)
-  description = "List of tags"
+  description = "Add user resource tags to the Transit Gateway instance to organize, track, and manage costs. [Learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#tag-types)."
   default     = null
+  validation {
+    condition     = alltrue([for tag in var.resource_tags : can(regex("^[A-Za-z0-9 _\\-.:]{1,128}$", tag))])
+    error_message = "Each resource tag must be 128 characters or less and may contain only A-Z, a-z, 0-9, spaces, underscore (_), hyphen (-), period (.), and colon (:)."
+  }
 }
 
 variable "vpc_connections" {
